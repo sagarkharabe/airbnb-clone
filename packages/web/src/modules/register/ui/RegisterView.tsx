68f53846972c1d42ede63b093dbccac9c1fc3964
sagarkharbe <sagarkharabe97@gmail.com>
2019-07-19 01:02:31 +0530

    sharing validation code between web app and graphql server

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from "react";
 import { Form, Icon, Input, Button } from "antd";
 import { withFormik, FormikErrors, FormikProps } from "formik";
 import * as yup from "yup";
+import { validUserSchema } from "@airbnb/common";
 interface FormValues {
   email: string;
   password: string;
@@ -72,26 +73,9 @@ class C extends PureComponent<FormikProps<FormValues> & Props> {
     );
   }
 }
-export const emailNotLongEnough = "email must be at least 3 characters";
-export const passwordNotLongEnough = "password must be at least 3 characters";
-export const invalidEmail = "email must be a valid email";
-
-const validationSchema = yup.object().shape({
-  email: yup
-    .string()
-    .min(3, emailNotLongEnough)
-    .max(255)
-    .email(invalidEmail)
-    .required(),
-  password: yup
-    .string()
-    .min(3, passwordNotLongEnough)
-    .max(255)
-    .required()
-});
 
 export const RegisterView = withFormik<Props, FormValues>({
-  validationSchema,
+  validationSchema: validUserSchema,
   validateOnChange: false,
   mapPropsToValues: () => ({ email: "", password: "" }),
   handleSubmit: async (values, { props, setErrors }) => {
